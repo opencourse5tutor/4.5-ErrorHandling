4a31a3188e5bc24c7b1e5f46ce6ab89ce6fbb2ae
opencourse5tutor <102296493+opencourse5tutor@users.noreply.github.com>
2022-04-21 13:10:56 +0530

    Add files via upload

---
+++ ErrorHandle1.js
@@ -0,0 +1,14 @@
+var fs = require('fs')
+
+try {
+console.log("This is the start of the try block")
+const data = fs.readFileSync('node.txt', 'utf8')
+console.log(data)
+console.log("This is the end of the try block")
+} catch (err) {
+  console.log(err)
+}
+
+console.log("This is the end of the try..catch example")
+
+
